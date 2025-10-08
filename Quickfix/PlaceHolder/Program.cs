a18b64247990ca74f61774511c7b88fc16bf30c3
SidewayHippo <pauljasper272@gmail.com>
2025-10-08 17:07:24 +0800

    Add files via upload

---
+++ Quickfix/PlaceHolder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cal
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            //this is use to hold string or int,double
+
+            string name = "John Wick", job = "Assassin", place = "New York", email = "unknown";
+            double salary =200000.00, cost = 80000.00;
+            int age = 48;
+
+            //executing code by applying the string int and double 
+            Console.WriteLine("Target Name; {0}\nRecent job: {1}\nRecent Location: {2}\nEmail of target: {3}\nAge: {4}\nReward :{5}\nParticipation Cost: {6}",name,job,place,email,age,salary,cost);
+        }
+    }
+}
