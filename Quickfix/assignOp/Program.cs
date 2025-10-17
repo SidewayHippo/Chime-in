d33c6a6204574231f2f596f0331486a1341150bb
SidewayHippo <pauljasper272@gmail.com>
2025-10-17 22:57:18 +0800

    Add files via upload

---
+++ Quickfix/assignOp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itsgood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //here we will do assignment operators(idk if im making sense, since i have 40 degree fever)
+            int a = 5;
+             a += 3;
+            int b = 10;
+            //+= 3 will compute to a (basically it's 5+3) you could use other operator such as -,*,/ and others
+
+            b += a;
+
+            Console.WriteLine($"answer: {a}");
+            //notice there's a $ and that means interpolated string however for this scenario you can compute despite being an interpolated string
+            Console.WriteLine($"another answer: {b}");
+        }
+    }
+}
